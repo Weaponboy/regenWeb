9a4a83ae8fa4a016c03be40fe0ef273c721f1fea
Weaponboy <joshandy06@gmail.com>
2025-06-02 12:58:04 +0200

    Your commit message here

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2618777231"
+    serviceWorkerVersion: "1493378738"
   }
 });
